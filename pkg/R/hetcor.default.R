055e6fee04d0c01c9a0e14d3f5571dee7fea36c7
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-25 15:46:24 +0000

    names for matrix input to hetcor() fixed. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/polycor@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hetcor.default.R
+++ pkg/R/hetcor.default.R
@@ -1,8 +1,10 @@
-# last modified 5 Dec 07 by J. Fox
+# last modified 25 January 2009 by J. Fox
 
-"hetcor.default" <-
-function(data, ..., ML=FALSE, std.err=TRUE, bins=4, pd=TRUE){
-  dframe <- data.frame(data, ...)
-  names(dframe)[1] <- deparse(substitute(data))
-  hetcor(dframe, ML=ML, std.err=std.err, bins=bins, pd=pd)
-  }
+hetcor.default <-
+	function (data, ..., ML = FALSE, std.err = TRUE, bins = 4, pd = TRUE) 
+{
+	dframe <- data.frame(data, ...)
+	if (!missing(...)) names(dframe)[1] <- deparse(substitute(data))
+	hetcor(dframe, ML = ML, std.err = std.err, bins = bins, pd = pd)
+}
+
