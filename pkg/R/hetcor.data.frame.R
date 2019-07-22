77e260d977ae255636cb4f4880d3e09869218178
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-07-22 12:39:13 +0000

    permit character data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/polycor@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hetcor.data.frame.R
+++ pkg/R/hetcor.data.frame.R
@@ -1,4 +1,4 @@
-# last modified 2016-10-05 by J. Fox
+# last modified 2019-07-22 by J. Fox
 
 "hetcor.data.frame" <-
     function(data, ML=FALSE, std.err=TRUE, use=c("complete.obs", "pairwise.complete.obs"),
@@ -7,6 +7,10 @@
             rho <- r*(1 + (1 - r^2)/(2*(n - 3))) # approx. unbiased estimator
             v <- (((1 - rho^2)^2)/(n + 6))*(1 + (14 + 11*rho^2)/(2*(n + 6)))
             sqrt(v)
+        }
+        if (any(sapply(data, function(x) inherits(x, "character")))){
+          message("data contain one or more character variables",
+                  "\nthe values of which are ordered alphabetically")
         }
         use <- match.arg(use)
         if (use == "complete.obs") data <- na.omit(data)
@@ -41,7 +45,8 @@
                         Test[i, j] <- pchisq(chisq(x, y, r, bins=bins), bins^2 - 2, lower.tail=FALSE)
                     }
                 }
-                else if (inherits(x, c("factor", "logical")) && inherits(y, c("factor", "logical"))) {
+                else if (inherits(x, c("factor", "logical", "character")) && 
+                         inherits(y, c("factor", "logical", "character"))) {
                     Type[i, j] <- Type[j, i] <- "Polychoric"
                     result <- try(polychor(x, y, ML=ML, std.err=std.err), silent=TRUE)
                     error <- inherits(result, "try-error")
@@ -61,12 +66,14 @@
                     else R[i, j] <- R[j, i] <- result
                 }
                 else {
-                    if (inherits(x, c("factor", "logical")) && inherits(y, c("numeric", "integer")))
+                    if (inherits(x, c("factor", "logical", "character")) && 
+                        inherits(y, c("numeric", "integer")))
                         result <- try(polyserial(y, x, ML=ML, std.err=std.err, bins=bins), silent=TRUE)
-                    else if (inherits(x, c("numeric", "integer")) && inherits(y, c("factor", "logical")))
+                    else if (inherits(x, c("numeric", "integer")) && 
+                             inherits(y, c("factor", "logical", "character")))
                         result <- try(polyserial(x, y, ML=ML, std.err=std.err, bins=bins), silent=TRUE)
                     else {
-                        stop("columns must be numeric, logical, or factors.")
+                        stop("columns must be numeric, factors, logical, or character.")
                     }
                     Type[i, j] <- Type[j, i] <- "Polyserial"
                     error <- inherits(result, "try-error")
